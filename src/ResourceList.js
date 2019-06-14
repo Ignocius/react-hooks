fb69ff2a7a0e448461ff81e2793ee800be865636
ignocius <ignocius@gmail.com>
2019-06-14 12:56:03 +0200

    class based resourcelist soon to be rewrtitten

--- src/ResourceList.js
+++ src/ResourceList.js
@@ -8,6 +8,14 @@ class ResourceList extends Component {
     const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
     this.setState({ resources: response.data })
   }
+
+  async componentDidUpdate(prevProps) {
+    if (prevProps.resource !== this.props.resource) {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+      this.setState({ resources: response.data })
+    }
+  }
+
   render() {
     return <div>{this.state.resources.length}</div>
   }
